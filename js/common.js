d184383add7062ccb59c8c9a61e6bc96a19d78fa
ruslankkush <kushakov5@gmail.com>
2017-02-10 19:23:35 +0400

    four commit

--- js/common.js
+++ js/common.js
@@ -12,20 +12,20 @@ $(document).ready(function(){
 	// 	$('body').toggleClass('in')
 	// });
 
-	$("body").niceScroll({
-    cursorcolor:"rgba(0, 151, 214, .5)",
-    cursorminheight: 10,
-    cursorwidth: 4,
-    touchbehavior: false,
-    bouncescroll: false,
-    // horizrailenabled: true,
-    scrollspeed: 100,
-    mousescrollstep: 50,
-    railpadding: {
-      right: 3
-    },
-    cursorborder: "0px"
-  });
+	// $("body").niceScroll({
+ //    cursorcolor:"rgba(0, 151, 214, .5)",
+ //    cursorminheight: 10,
+ //    cursorwidth: 4,
+ //    touchbehavior: false,
+ //    bouncescroll: false,
+ //    // horizrailenabled: true,
+ //    scrollspeed: 100,
+ //    mousescrollstep: 50,
+ //    railpadding: {
+ //      right: 3
+ //    },
+ //    cursorborder: "0px"
+ //  });
 
     // ==scroll navbar
     // arrowTop();
@@ -36,4 +36,17 @@ $(document).ready(function(){
     //       ($(this).scrollTop() > offset) ? $back_to_top.addClass('fixed') : $back_to_top.removeClass('fixed');
     //     });
     // }
+
+
+
+    $('#exampleModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) 
+        var recipient = button.data('whatever') 
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').val(recipient)
+    });
+
+
+
 });
