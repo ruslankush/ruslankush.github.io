dcb0d68402f0c33b4530eb0672a00f6e09f55436
ruslankush <kushakov5@gmail.com>
2024-12-02 15:21:13 +0300

    LYNX

---
+++ LYNX/js/normalize.js
@@ -0,0 +1,27 @@
+$(function() {
+	
+	$('.accordion-name').click(function (e) {
+	
+		e.preventDefault();
+	
+		$(".accordion-body").not($(this).next()).slideUp(400);
+	
+		$(this).next().slideToggle(400);
+	
+		$(".accordion-item").not($(this).closest(".accordion-item")).removeClass("active");
+	
+		$(this).closest(".accordion-item").toggleClass("active");
+	
+	});
+
+	$('.js-ban').each(function() {
+
+		$(this).length;
+		var banImg = $(this).find('img').attr('src');
+
+		$(this).attr('style', 'background-image: url(../'+ banImg +')');
+
+	});
+
+
+});
