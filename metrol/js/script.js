dddac7e28dc92544330f67b5c120736d20836894
ruslankush <kushakov5@gmail.com>
2025-01-20 03:16:17 +0300

    metrol

---
+++ metrol/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function addScript(src){
+  var script = document.createElement('script');
+  script.src = src;
+  script.async = false; 
+  document.head.appendChild(script);
+}
+
+addScript('libs/jquery3.1/jquery3.1.min.js'); 
+addScript('libs/slick.js/slick.min.js');
+addScript('js/normalize.js');
